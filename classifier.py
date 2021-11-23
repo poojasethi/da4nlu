aa5af4819bf8ac8d30d38bdfd403a5b7c783a147
Pooja Sethi <sethi.pooja@gmail.com>
2021-11-22 21:03:35 -0800

    Initial classifier

--- classifier.py
+++ classifier.py
@@ -1,5 +1,13 @@
 from transformers import pipeline
+from wilds import get_dataset
+from wilds.common.data_loaders import get_train_loader
 
+
+dataset = get_dataset(dataset='civilcomments', download=False)
+
+
+
+# Get model predictions
 classifier = pipeline('sentiment-analysis')
 prediction = classifier("Wow, using HuggingFace is easy")
 print(prediction)
