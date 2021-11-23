3a5995ef007c0ce80fc440c1af29f051980c3a93
Pooja Sethi <sethi.pooja@gmail.com>
2021-11-22 23:37:54 -0800

    Setup train and eval loaders

--- classifier.py
+++ classifier.py
@@ -2,10 +2,23 @@ from transformers import pipeline
 from wilds import get_dataset
 from wilds.common.data_loaders import get_train_loader
 
+DATASET = 'civilcomments'
 
-dataset = get_dataset(dataset='civilcomments', download=False)
+TRAIN_SPLIT = 'train'
+EVAL_SPLIT = 'eval'
+
+STANDARD_LOADER = 'standard'
+
+dataset = get_dataset(dataset=DATASET, download=False)
+
+train_data = dataset.get_subset(TRAIN_SPLIT, frac=0.01)
+train_loader = get_train_loader(STANDARD_LOADER, train_data, batch_size=16)
+for x, y_true, metadata in train_loader:
+    print(f"{x}, {y_true}, {metadata}")
 
 
+eval_data = dataset.get_subset(EVAL_SPLIT, frac=1.0)
+eval_loader = get_eval_loader(STANDARD_LOADER, eval_data, batch_size=100)
 
 # Get model predictions
 classifier = pipeline('sentiment-analysis')
