ffdb821472d23de42d1e90c8eff5d6e0bb2514c6
Pooja Sethi <sethi.pooja@gmail.com>
2021-12-01 13:17:15 -0800

    Extract worst-case accuracy

--- accuracy_extractor.py
+++ accuracy_extractor.py
@@ -5,8 +5,9 @@ from typing import Dict, OrderedDict
 
 # Expand each subdirectory and get the confidence scores.
 
-CIVIL_COMMENTS_TEST_REGEX = "Results for: test\nAverage acc: (0.\d+)"
-AMAZON_TEST_REGEX = "TODO"
+TEST_AVG_ACCURACY_REGEX = "Results for: test\nAverage acc: (0.\d+)"
+WORST_GROUP_ACCURACY_REGEX = "Results for: test(.|\n)*(Worst-group acc:) (0.\d+)*" 
+TENTH_PERCENTILE_ACCURACY_REGEX = "Results for: test(.|\n)*(Worst-group acc:) (0.\d+)*" 
 
 """
 Prints out the results in a pretty-format for LaTeX. 
@@ -26,11 +27,17 @@ def get_immediate_subdirectories(a_dir):
             if os.path.isdir(os.path.join(a_dir, name))]
 
 
-def find_accuracy(text: str) -> float:
-    match = re.search(CIVIL_COMMENTS_TEST_REGEX, text)
+def find_accuracy(text: str, regex: str) -> float:
+    match = re.search(regex, text)
     if not match:
         raise ValueError("Did not find accuracy!")
-    return float(match.groups()[0])
+
+    if regex == TEST_AVG_ACCURACY_REGEX:    
+        return float(match.groups()[0])
+    elif regex == WORST_GROUP_ACCURACY_REGEX:
+        return float(match.groups()[2])
+    else:
+        raise ValueError("Did not recognize regex")
 
 def latex_format_results(results: Dict[str, Dict[int, float]]) -> str:
     pretty_results = ""
@@ -39,33 +46,32 @@ def latex_format_results(results: Dict[str, Dict[int, float]]) -> str:
         pretty_results_line = f"{strategy.replace('_', ' ').title()}"
         for iteration, accuracy in accuracies.items():
             pretty_results_line += f" & {accuracy}" 
-        pretty_results += f"{pretty_results_line}\n"
+        pretty_results += f"{pretty_results_line} \\\\\n"
     
     return pretty_results
 
-def main():
-    config = get_config()
-    results = os.walk(config.results_path)
+def get_accuracy_results(results_path: str, regex: str):
+    results = os.walk(results_path)
     results = OrderedDict()
 
-    iteration_dirnames = get_immediate_subdirectories(config.results_path)
+    iteration_dirnames = get_immediate_subdirectories(results_path)
    
     for iteration in iteration_dirnames: 
-        strategy_dirnames = get_immediate_subdirectories(os.path.join(config.results_path, iteration))
+        strategy_dirnames = get_immediate_subdirectories(os.path.join(results_path, iteration))
 
         for strategy in strategy_dirnames:
-            with open(os.path.join(config.results_path, iteration, strategy, "results.txt"), 'r') as fh:
+            with open(os.path.join(results_path, iteration, strategy, "results.txt"), 'r') as fh:
                 text = fh.read()
                 if strategy not in results:
                     results[strategy] = {}
-                results[strategy][int(iteration)] = find_accuracy(text)
+                results[strategy][int(iteration)] = find_accuracy(text, regex)
     
 
     # Go back and populate the accuracy for the 0th iteration.
     zero_iteration_accuracy = float('nan')
-    with open(os.path.join(config.results_path, "0", "results.txt"), 'r') as fh:
+    with open(os.path.join(results_path, "0", "results.txt"), 'r') as fh:
         text = fh.read()
-        zero_iteration_accuracy = find_accuracy(text)
+        zero_iteration_accuracy = find_accuracy(text, regex)
     
 
     for strategy in results.keys():
@@ -73,7 +79,18 @@ def main():
         iterations[0] = zero_iteration_accuracy
         results[strategy] = OrderedDict(sorted(iterations.items()))
     
-    print(latex_format_results(results))
+    return results
+
+def main():
+    config = get_config()
+    average_accuracy = get_accuracy_results(config.results_path, TEST_AVG_ACCURACY_REGEX)
+    worst_group_accuracy = get_accuracy_results(config.results_path, WORST_GROUP_ACCURACY_REGEX) 
+    print("Test Average Accuracy")
+    print(latex_format_results(average_accuracy))
+    print()
+
+    print("Worst-Group Accuracy")
+    print(latex_format_results(worst_group_accuracy))
         
 
 if __name__ == "__main__":
